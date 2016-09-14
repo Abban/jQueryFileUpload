de6ad5bb18f5ead9c5c394bc0e143a25c34c4bfa
Tomasz Miller <peniakoff@users.noreply.github.com>
2016-09-14 17:48:41 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,105 +1,67 @@
-$(function()
-{
-	// Variable to store your files
-	var files;
-
-	// Add events
-	$('input[type=file]').on('change', prepareUpload);
-	$('form').on('submit', uploadFiles);
-
-	// Grab the files and set them to our variable
-	function prepareUpload(event)
-	{
-		files = event.target.files;
-	}
-
-	// Catch the form submit and upload the files
-	function uploadFiles(event)
-	{
-		event.stopPropagation(); // Stop stuff happening
-        event.preventDefault(); // Totally stop stuff happening
-
-        // START A LOADING SPINNER HERE
-
-        // Create a formdata object and add the files
-		var data = new FormData();
-		$.each(files, function(key, value)
-		{
-			data.append(key, value);
-		});
-        
-        $.ajax({
-            url: 'submit.php?files',
-            type: 'POST',
-            data: data,
-            cache: false,
-            dataType: 'json',
-            processData: false, // Don't process the files
-            contentType: false, // Set content type to false as jQuery will tell the server its a query string request
-            success: function(data, textStatus, jqXHR)
-            {
-            	if(typeof data.error === 'undefined')
-            	{
-            		// Success so call function to process the form
-            		submitForm(event, data);
-            	}
-            	else
-            	{
-            		// Handle errors here
-            		console.log('ERRORS: ' + data.error);
-            	}
-            },
-            error: function(jqXHR, textStatus, errorThrown)
-            {
-            	// Handle errors here
-            	console.log('ERRORS: ' + textStatus);
-            	// STOP LOADING SPINNER
+/**
+ * JS file for manage the files
+ */
+var files;
+$('input[type=file]').on('change', prepareUpload);
+function prepareUpload(event) {
+  files = event.target.files;
+}
+$('form#upload_file').on('submit', uploadFiles);
+function uploadFiles(event) {
+	event.stopPropagation();
+	event.preventDefault();
+    document.getElementById("loader").style.display = "block";
+    var data = new FormData();
+    $.each(files, function(key, value) {
+    	data.append(key, value);
+    });
+    $.ajax({
+        url: 'upload.php?files',
+        type: 'POST',
+        data: data,
+        cache: false,
+        dataType: 'json',
+        processData: false,
+        contentType: false,
+        success: function(data, textStatus, jqXHR) {
+            if(typeof data.error === 'undefined') {
+                submitForm(event, data);
+            } else {
+                console.log('ERRORS: ' + data.error);
             }
-        });
-    }
-
-    function submitForm(event, data)
-	{
-		// Create a jQuery object from the form
-		$form = $(event.target);
-		
-		// Serialize the form data
-		var formData = $form.serialize();
-		
-		// You should sterilise the file names
-		$.each(data.files, function(key, value)
-		{
-			formData = formData + '&filenames[]=' + value;
-		});
-
-		$.ajax({
-			url: 'submit.php',
-            type: 'POST',
-            data: formData,
-            cache: false,
-            dataType: 'json',
-            success: function(data, textStatus, jqXHR)
-            {
-            	if(typeof data.error === 'undefined')
-            	{
-            		// Success so call function to process the form
-            		console.log('SUCCESS: ' + data.success);
-            	}
-            	else
-            	{
-            		// Handle errors here
-            		console.log('ERRORS: ' + data.error);
-            	}
-            },
-            error: function(jqXHR, textStatus, errorThrown)
-            {
-            	// Handle errors here
-            	console.log('ERRORS: ' + textStatus);
-            },
-            complete: function()
-            {
-            	// STOP LOADING SPINNER
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('ERRORS: ' + textStatus);
+            document.getElementById("loader").style.display = "none";
+        }
+    });
+}
+function submitForm(event, data) {
+    $form = $(event.target);
+    var formData = $form.serialize();
+    $.each(data.files, function(key, value) {
+        formData = formData + '&filenames[]=' + value;
+    });
+    $.ajax({
+        url: 'upload.php',
+        type: 'POST',
+        data: formData,
+        cache: false,
+        dataType: 'json',
+        success: function(data, textStatus, jqXHR) {
+            if(typeof data.error === 'undefined') {
+                console.log('SUCCESS: ' + data.success);
+            } else {
+                console.log('ERRORS: ' + data.error);
             }
-		});
-	}
-});
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('ERRORS: ' + textStatus);
+            document.getElementById("loader").style.display = "none";
+        },
+        complete: function() {
+            document.getElementById("loader").style.display = "none";
+            alert('Plik został pomyślnie zapisany!');
+        }
+    });
+}
