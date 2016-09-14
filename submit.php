83a8d55ac70c5051bc1e161aaab0c23c425b24a3
Tomasz Miller <peniakoff@users.noreply.github.com>
2016-09-14 17:44:54 +0200

    Update submit.php

--- submit.php
+++ submit.php
@@ -1,31 +1,20 @@
-<?php // You need to add server side validation and better error handling here
-
+<?php
 $data = array();
-
-if(isset($_GET['files']))
-{	
-	$error = false;
-	$files = array();
-
-	$uploaddir = './uploads/';
-	foreach($_FILES as $file)
-	{
-		if(move_uploaded_file($file['tmp_name'], $uploaddir .basename($file['name'])))
-		{
-			$files[] = $uploaddir .$file['name'];
-		}
-		else
-		{
-		    $error = true;
-		}
-	}
-	$data = ($error) ? array('error' => 'There was an error uploading your files') : array('files' => $files);
+if (isset($_GET['files'])) {
+    $error = false;
+    $files = array();
+    $uploaddir = './../../files/';
+    foreach ($_FILES as $file) {
+        if (move_uploaded_file($file['tmp_name'], $uploaddir .basename($file['name']))) {
+            $files[] = $uploaddir .$file['name'];
+        }
+        else {
+            $error = true;
+        }
+    }
+    $data = ($error) ? array('error' => 'There was an error uploading your files') : array('files' => $files);
 }
-else
-{
-	$data = array('success' => 'Form was submitted', 'formData' => $_POST);
+else {
+    $data = array('success' => 'Form was submitted', 'formData' => $_POST);
 }
-
 echo json_encode($data);
-
-?>
